a8fd3216ba432b554b38651d9c74d5c80667d64c
Andrii <atrybushnyi@kse.org.ua>
2025-04-08 15:07:15 +0300

    f

--- DataAcquisitionProject/src/DataAcquisition.Core/Interfaces/IDataProvider.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace DataAcquisition.Core.Interfaces
-{
-	public interface IDataProvider<T>
-	{
-		Task<T> GetDataAsync(string source);
-	}
-}
--- DataAcquisitionProject/src/DataAcquisition.Core/Interfaces/IDataTransformer.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace DataAcquisition.Core.Interfaces
-{
-	public interface IDataTransformer<TSource, TTarget>
-	{
-		IEnumerable<TTarget> Transform(TSource source);
-	}
-}
